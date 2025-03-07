5e1bd1aa50e0ea2611803007c0ebfbc064205971
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-03-07 12:00:01 +0530

    JS-BASICS

---
+++ Week-1/http-server-1/index.js
@@ -0,0 +1,14 @@
+const express=require("express");
+
+const app=express()
+
+const port=3001
+
+app.get('/',(res,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at ${https://localhost:3000}`)
+})
+
