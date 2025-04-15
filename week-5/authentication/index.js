94a4cbe7c8e3285b954ad5fb3326115a8325ae77
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-15 11:33:56 +0530

    replace token logic with jwt

--- week-5/authentication/index.js
+++ week-5/authentication/index.js
@@ -1,25 +1,28 @@
 const express = require("express");
+const jwt=require("jsonwebtoken")
+const JWT_SCERET="JWT_SCERET"
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-function generateToken(){
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-         'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2',
-          '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken(){
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+//         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+//         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+//          'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2',
+//           '3', '4', '5', '6', '7', '8', '9'];
 
-        let token=''
+//         let token=''
 
-        for (let i=0;i<=options.length;i++){
-            token+=options[Math.floor(Math.random()*options.length)]
-        }
-        return token
+//         for (let i=0;i<=options.length;i++){
+//             token+=options[Math.floor(Math.random()*options.length)]
+//         }
+//         return token
+// }
 
-}
+// replacing token logic to jsonwebtoken 
 
 app.post("/signup", (req, res) => {
   const username = req.body.username;
@@ -39,13 +42,15 @@ app.post("/signin", (req, res) => {
     const user=users.find((user)=> user.username===username && user.password===password)
 
     if (user){
-        const token=generateToken()
+        const token=jwt.sign({
+          username:user.username
+        },JWT_SCERET)
         user.token=token
 
         res.send({
             token
         })
-        console.log(user)
+        console.log(users)
     }else{
         res.status(403).send({
             msg:"Invalid username and passward"
@@ -53,6 +58,24 @@ app.post("/signin", (req, res) => {
     }
 
 });
+
+app.get('/me',(req,res)=>{
+  const token=req.headers.token
+  const userDetails=jwt.verify(token,JWT_SCERET)
+
+  const username=userDetails.username
+  const user=users.find((tkn)=> tkn.token===token)
+
+  if(user){
+    res.json({
+      username:user.username,
+      password:user.password
+    })
+  }else{
+    res.json("inavlid account or user not singned up")
+  }
+
+})
 
 app.listen(4001, () => {
   console.log("Server running on http://localhost:4001");
